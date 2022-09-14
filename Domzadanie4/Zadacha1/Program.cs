3b3a24b1fee47ca124106d9e47388c4810894423
Andrey <ndrjkolosovskij@gmail.com>
2022-09-14 17:13:29 +0300

    D4z1

---
+++ Domzadanie4/Zadacha1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int A = Promt("Введите А");
+int B = Promt("Введите Б");
+System.Console.WriteLine(Math.Pow(A, B));
+int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    return Convert.ToInt32(value);
+}
