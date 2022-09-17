5ec4bfa6c8c5d00dad028139bd96c55171099f57
Andrey <ndrjkolosovskij@gmail.com>
2022-09-18 00:15:19 +0300

    D5z2

---
+++ Domzadanie5/Zadacha2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+/// Я поняла это как "Найдите сумму элементов с нечетным индексом"
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 100);
+}
+
+System.Console.WriteLine("длина массива");
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[lenght];
+FillArray(array);
+System.Console.WriteLine("[{0}]", string.Join(", ", array));
+int Summ = 0;
+for (int i = 1; i < array.Length; i = i + 2)
+    Summ += array[i];
+System.Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях {Summ}");
